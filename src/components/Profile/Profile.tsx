df3e0b372d64c31b9cc7c031e030b9df5b5b6fc2
Samuel Raducan <samuel.thembp@gmail.com>
2024-03-24 15:03:01 +0000

    Add extra profile information and styles

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,3 +1,4 @@
+import "./Profile.scss";
 import { useParams } from "react-router-dom";
 import { Team } from "../../types/team";
 
@@ -15,10 +16,34 @@ const Profile = ({ team }: ProfileProps) => {
     if (!member) return;
 
     return (
-        <div>
-            <h1>{member.name}</h1>
-            <p>Role {member.role}</p>
-        </div>
+        <>
+            <h1 className="profile-heading">Profile Information</h1>
+            <div className="profile-card">
+                <img
+                    src={member.profile.profilePicture}
+                    alt={member.name}
+                    className="profile-card__image"
+                />
+                <div className="profile-card__info">
+                    <h1 className="profile-card__name">{member.name}</h1>
+                    <p className="profile-card__role">{member.role}</p>
+                    <div className="profile-card__details">
+                        <p>
+                            <strong>Experience:</strong>{" "}
+                            {member.profile.experience}
+                        </p>
+                        <p>
+                            <strong>Department:</strong>{" "}
+                            {member.profile.department}
+                        </p>
+                        <p>
+                            <strong>Tech stack:</strong>{" "}
+                            {member.profile.techstack.join(", ")}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 };
 
